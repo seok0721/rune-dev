294c75a73931803499664ef2e1500ba7655429af
Wangseok Lee <seok0721@gmail.com>
2016-10-23 11:07:46 +0900

    add install packages scriptw

---
+++ 98-install-packages.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+apt-get -y install \
+  apache2\
+  chrony\
+  git\
+  glance\
+  keystone\
+  libapache2-mod-wsgi\
+  mariadb-server\
+  matiadb-server\
+  memcached\
+  mongodb-clients\
+  mongodb-server\
+  nova-api\
+  nova-conductor\
+  nova-consoleauth\
+  nova-novncproxy\
+  nova-scheduler\
+  openssl\
+  python-memcache\
+  python-openstackclient\
+  python-pymongo\
+  python-pymysql\
+  python-pymysqlFROM\
+  rabbitmq-server\
+  vim
